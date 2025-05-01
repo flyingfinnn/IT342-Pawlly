0cf11df91e0893a4a6e732608509b644df0c2221
Tadashi10x <rigelbaltazar2@gmail.com>
2025-05-01 22:08:20 +0800

    ADDED JAR FILE DIRECTLY

--- backend/src/test/java/com/g1appdev/Hubbits/HubbitsApplicationTests.java
+++ backend/src/test/java/com/g1appdev/Hubbits/HubbitsApplicationTests.java
@@ -3,6 +3,7 @@ package com.g1appdev.Hubbits;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class HubbitsApplicationTests {
 
